8e3d972d3469c70d873ffb55ced51a0957d7b6f9
alliejant <alliejant@gmail.com>
2018-11-12 23:28:25 -0800

    set up basics of a responsive app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          Alexandra Antkowiak
+        <img src={require("./images/headshot.jpg")} alt="headshot" className="circle" />
+        <header> 
+          <h1>Alexandra Antkowiak</h1>
+          <span>
+            <i className="fab fa-github"></i>
+            <i className="fab fa-linkedin"></i>
+            <i className="fab fa-angellist"></i>
+          </span>
         </header>
+        <nav>
+          <i className="fas fa-bars"></i>
+          <span>Contact</span>
+          <span>About</span>
+          <span>Skills</span>
+          <span>Work</span>
+          <span>Resume</span>
+        </nav>
+        <main>
+          Blank Text
+        </main>
       </div>
     );
   }
