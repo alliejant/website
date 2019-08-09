aba62f1602773b4e4943c96afb23e08ed8045c9a
alliejant <alliejant@gmail.com>
2019-08-09 14:17:39 -0700

    added Work section and styling

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,9 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Work.scss";
 
 class Work extends Component {
   render() {
     return (
-      <p> Work </p>
+      <div className="Work">
+        <p> I am looking for work that is:</p>
+        <ul>
+          <li>
+            <strong>Part-time</strong> (20-30 hrs/wk)
+          </li>
+          <li>
+            <strong>Remote</strong> (can travel for team meetings when required)
+          </li>
+        </ul>
+        <p>
+          Both large companies and start-ups interest me and I and can do
+          front-end, back-end, and full-stack work for both web and mobile. I am
+          most well-versed in Javascript and have worked with both React and
+          React Native. I love learning and would be happy to learn new
+          languages and frameworks on the job.
+        </p>
+        <p>
+          In my previous positions, I have shown that I work well independently
+          but can communicate well in a group. I am committed to doing what I
+          can in order to present my best work. Please{" "}
+          <Link to="/contact"> contact me</Link> if you think I would be a good
+          fit for your team.
+        </p>
+      </div>
     );
   }
 }
