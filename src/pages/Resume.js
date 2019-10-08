626f8977325cf5a5829867114e92fb03382ba724
alliejant <alliejant@gmail.com>
2019-10-08 16:57:40 -0400

    added Resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "../styles/pages/Resume.scss";
 
 class Resume extends Component {
   render() {
     return (
-      <p> Resume </p>
+      <iframe
+        src={require("../media/Alexandra-Garcia-Resume.pdf")}
+        title="resume"
+      />
     );
   }
 }
