550e41d93f59ba4ae2f8a9546bb16cfd7b9d5192
alliejant <alliejant@gmail.com>
2019-10-08 16:59:21 -0400

    Added Contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,112 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import IconButton from "../components/IconButton";
+import axios from "axios";
+import "../styles/pages/Contact.scss";
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contactInfo: "",
+      message: "",
+      feedback: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleButtonClick(type) {
+    let message = "";
+    switch (type) {
+      case "job":
+        message = `Hi Allie,\n\nI have a job you might be interested in! We are working on _____.\n\nBest,\n`;
+        break;
+      case "study":
+        message = `Hi Allie,\n\nI'm studying _____ and would love to meet up to study/build apps together. What I'm thinking is we can ______. Let me know if you are interested!\n\nBest,\n`;
+        break;
+      default:
+    }
+    this.setState({ feedback: "", message });
+  }
+
+  handleChange(event) {
+    let newState = {};
+    newState[event.target.name] = event.target.value;
+
+    this.setState({ feedback: "", ...newState });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.state.contactInfo.length < 7 || this.state.message.length < 7) {
+      this.setState({
+        feedback: "Please fill in your contact info and a message"
+      });
+      return;
+    }
+    axios.defaults.headers.post["Content-Type"] = "application/json";
+    axios
+      .post("https://www.enformed.io/29rudjf3/", this.state)
+      .then(response => {
+        console.log("submitted");
+        this.setState({
+          contactInfo: "",
+          message: "",
+          feedback: "Thank you! I'll get back to you as soon as possible"
+        });
+      })
+      .catch(error => {
+        this.setState({
+          feedback:
+            "We were unable to submit your message. Try again or contact me on LinkedIn or Angelist!"
+        });
+      });
+  }
+
   render() {
+    const feedback = this.state.feedback ? this.state.feedback : "";
+    console.log(feedback);
     return (
-      <p> Contact </p>
+      <div className="Contact">
+        <ul>
+          <IconButton
+            icon="fas fa-user-tie icon-2x"
+            text="I have work for you"
+            action={() => this.handleButtonClick("job")}
+          />
+          <IconButton
+            icon="fas fa-user-friends"
+            text="I want to study together"
+            action={() => this.handleButtonClick("study")}
+          />
+          <IconButton
+            icon="fas fa-user-astronaut"
+            text="Reset the form"
+            action={() => this.handleButtonClick("reset")}
+          />
+        </ul>
+        <form onSubmit={this.handleSubmit} className="Contact">
+          <p className="feedback">{feedback}</p>
+          <label>Contact Info:</label>
+          <input
+            type="text"
+            name="contactInfo"
+            placeholder="How should I reach you?"
+            value={this.state.contactInfo}
+            onChange={this.handleChange}
+          />
+          <label>Message:</label>
+          <textarea
+            name="message"
+            placeholder="Enter your message"
+            value={this.state.message}
+            onChange={this.handleChange}
+          />
+          <input type="hidden" name="*honeypot" />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     );
   }
 }
