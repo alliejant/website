106fbe1970e068ab72ce2118098444d8f623b278
alliejant <alliejant@gmail.com>
2019-10-08 16:58:44 -0400

    modified IconLink to remove attributes not using

--- src/components/IconLink.js
+++ src/components/IconLink.js
@@ -2,12 +2,9 @@ import React, { Component } from "react";
 
 class IconLink extends Component {
   render() {
-    let icon = this.props.icon ? <i className={this.props.icon} /> : "";
-    let text = this.props.text ? this.props.text : "";
     return (
       <a href={this.props.link} target="_blank" rel="noopener noreferrer">
-        {icon}
-        {text}
+        <i className={this.props.icon} />
       </a>
     );
   }
